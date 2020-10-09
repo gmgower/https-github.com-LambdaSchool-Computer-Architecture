5c122b7e19451dff2efdb49fad029a3cb7d7ec62
@gmgower <gmgower@gmail.com>
2020-10-08 22:52:45 -0500

    call

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,18 @@
 
 import sys
 
+# program_filename = sys.argv[1]
+# print(program_filename)
+# sys.exit()
+# print(sys.argv)
+# sys.exit()
+
+# LDI = 0b10000010
+# MUL = 0b10100010
+# PRN = 0b01000111
+# HLT = 0b00000001
+
+
 class CPU:
     """Main CPU class."""
 
@@ -24,15 +36,21 @@ class CPU:
         self.PUSH = 0b01000101
         self.POP =  0b01000110
         self.HLT =  0b00000001
+        self.CALL = 0b1010000
+        self.RET = 0b00010001
+        self.ADD = 0b10100000
 
-        # Turning the branch table into an object to be able to update easier
+        # Turning the branch table into a dictionary to be able to update easier
         self.branchtable = {
             self.LDI: self.ldi,
             self.MUL: self.multiply,
             self.PRN: self.prn,
             self.HLT: self.halt,
             self.PUSH: self.push,
-            self.POP: self.pop            
+            self.POP: self.pop, 
+            self.CALL: self.call,
+            self.RET: self.ret,
+            self.ADD: self.addition          
         }
 
     def load(self, program_filename):
@@ -54,6 +72,22 @@ class CPU:
                 self.ram[address] = int(line, base=2)
 
                 address += 1  # add one and goes to the next
+
+    # For now, we've just hardcoded a program:
+
+    # program = [
+    #     # From print8.ls8
+    #     0b10000010, # LDI R0,8
+    #     0b00000000,
+    #     0b00001000,
+    #     0b01000111, # PRN R0
+    #     0b00000000,
+    #     0b00000001, # HLT
+    # ]
+
+    # for instruction in program:
+    #     self.ram[address] = instruction
+    #     address += 1
 
     # MAR contains the address that is being read or written to.
     #ram_read() should accept the address (MAR) to read and return the value stored #there.
@@ -84,6 +118,12 @@ class CPU:
         num_reg_a = self.ram_read(self.pc + 1)
         num_reg_b = self.ram_read(self.pc + 2)
         self.alu('MULT', num_reg_a, num_reg_b)
+        self.pc += 3
+
+    def addition(self):
+        num_reg_a = self.ram_read(self.pc + 1)
+        num_reg_b = self.ram_read(self.pc + 2)
+        self.alu('ADD', num_reg_a, num_reg_b)
         self.pc += 3
 
     def prn(self):
@@ -121,9 +161,31 @@ class CPU:
 
         self.pc += 2
 
+    def call(self):
+        # Get the address after the call so know where to return 
+        return_address = self.pc + 2
+
+        #push on the stack using stack pointer
+        self.reg[self.SP] -= 1
+        self.ram[self.reg[self.SP]] = return_address
+
+        # set the PC to the value in the given register
+        register_num = self.ram[self.pc + 1]
+        destination_address = self.reg[register_num]
+
+        # Sets the program counter to the destination address
+        self.pc = destination_address
+
+    def ret(self):
+        # pop return address from top of the stack
+        return_address = self.ram[self.reg[self.SP]]
+        self.reg[self.SP] += 1
+
+        # set the pc so it know where to return to 
+        self.pc = return_address
+    
     def halt(self):
-        self.running = False
-        
+        self.running = False        
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
